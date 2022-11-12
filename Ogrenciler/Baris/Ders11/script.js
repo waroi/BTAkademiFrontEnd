00c97eb8f14c70329358c758f27e54e9ab5b59c4
bbssyl <bb.sosyal@pm.me>
2022-11-12 11:21:43 +0300

    update ders11

---
+++ Ogrenciler/Baris/Ders11/script.js
@@ -0,0 +1,25 @@
+//Konsol ekranında mesaj gösterme
+// console.log("Merhaba");
+
+//Variables - Değişkenler
+
+//Number - Sayısal ifadeler
+// var firstNumber=20;
+// var secondNumber=30;
+// console.log(firstNumber+secondNumber);
+
+//Stringler - Metinsel ifadeler - Karakter Dizileri
+// var userName = "Barış";
+// console.log(userName);
+// console.log(typeof userName);
+
+//Boolean - True/False
+// var a = true;
+// console.log(a);
+// console.log(typeof a);
+
+//null - Boş
+// var a = null;
+// console.log(a);
+// console.log(typeof a);
+
