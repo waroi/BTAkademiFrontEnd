68bdfad453a77e0be1ede44ca4a806f0f5f51d6f
bbssyl <bb.sosyal@pm.me>
2022-11-13 20:44:35 +0300

    update JS TAHMİN
    çalışmaya başladığında HTML yükleme sorunu button'a event ekleyerek çözüldü.

--- Ogrenciler/Baris/0Workspace/JS-ODEV/Tahmin/scripts.js
+++ Ogrenciler/Baris/0Workspace/JS-ODEV/Tahmin/scripts.js
@@ -3,30 +3,41 @@ let puan = 100;
 let rastgeleSayi = Math.floor(Math.random() * 100);
 let tahmin;
 
-alert(`
+function calistir() {
+  alert(`
 Oyun Başlıyor...
 Başlamadan önceki toplam puanınız: ${puan}
 
 *** 0 ile 100 arasındaki bir sayıyı tahmin etmeye çalış.
 *** Konsol ekranındaki ne yöne gitmen gereken uyarıları dikkate almayı unutma!
 `);
-
-while (true) {
-  tahmin = Number(prompt("Tahmin girin: "));
-  if (rastgeleSayi == tahmin) {
-    alert(`Tebrikler sayıyı buldunuz!
+  while (true) {
+    tahmin = Number(
+      prompt(`
+  Kalan tahmin hakkı: ${20 - tahminSayi}
+  Tahmin: `)
+    );
+    if (rastgeleSayi == tahmin) {
+      alert(`Tebrikler sayıyı buldunuz!
           Tahmin sayısı: ${tahminSayi}
           Puan: ${puan}
           `);
-    break;
-  } else {
-    tahminSayi++;
-    puan -= 5;
-  }
+      break;
+    } else {
+      tahminSayi++;
+      puan -= 5;
 
-  if (rastgeleSayi < tahmin) {
-    console.log("Aşağı");
-  } else {
-    console.log("Yukarı");
+      if (rastgeleSayi < tahmin) {
+        console.log("Aşağı");
+      } else if (puan == 0) {
+        alert(`Maalesef sayıyı gerekli puan içinde tahmin edemediniz
+      Tahmin sayısı: ${tahminSayi}
+      Puan: ${puan}
+      `);
+        break;
+      } else {
+        console.log("Yukarı");
+      }
+    }
   }
 }
