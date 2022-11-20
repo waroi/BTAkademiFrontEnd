b34594b2f59e47c8ad5c65588c64abb5402ee31c
hazalecevit <hazalecevit@gmail.com>
2022-11-20 09:25:49 +0100

    a

--- Ogrenciler/Hürkan/Ders13-JS-Uyg/list.js
+++ Ogrenciler/Hürkan/Ders13-JS-Uyg/list.js
@@ -16,7 +16,26 @@ function select() {
 
 dv = document.getElementById('col-left');
 
-dv.addEventListener('dragstart', function(e) {
+dv.addEventListener('dragstart', function(e.) {
     dv.style.opacity=0.6;
     e.dataTransfer.dropEffect="move";
 });
+
+// Make a list
+const ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+const li1 = document.createElement('li');
+const li2 = document.createElement('li');
+ul.appendChild(li1);
+ul.appendChild(li2);
+
+function hide(evt) {
+  // evt.target refers to the clicked <li> element
+  // This is different than evt.currentTarget, which would refer to the parent <ul> in this context
+  evt.target.style.visibility = 'hidden';
+}
+
+// Attach the listener to the list
+// It will fire when each <li> is clicked
+ul.addEventListener('click', hide, false);
