96f42d988a584ad861188755477212ecaa059fdb
bbssyl <bb.sosyal@pm.me>
2022-11-27 11:13:11 +0300

    update ders16

---
+++ Ogrenciler/Baris/Ders16-ES6/06-Map-Set/app.js
@@ -0,0 +1,63 @@
+//MAP == Key(anahtar) - Value(değer)
+
+// const key1 = "Barış";
+// const key2 = { a: 10, b: 20 };
+// const key3 = () => 5;
+
+// let denemeMap = new Map(); //oluşturma
+// //DEĞER EKLEME
+
+// denemeMap.set(key1, "ilk değer");
+// denemeMap.set(key2, "ikinci değer");
+// denemeMap.set(key3, "üçüncü değer");
+
+// console.log(denemeMap);
+// console.log(typeof denemeMap);
+
+//değer okuma
+
+// console.log(denemeMap.get("Barış"));
+// console.log(denemeMap.get(key2));
+// console.log(denemeMap.get(key3));
+
+// denemeMap.forEach((value, key) => {
+//   console.log(key, value);
+// });
+
+// const students = new Map();
+// students.set("Barış", 90);
+// students.set("Hakan", 95);
+// students.set("Esma", 100);
+
+//foreach
+
+// students.forEach((value, key) => console.log(value, key));
+
+// for (let [key, value] of students) {
+//   console.log(key, value);
+// }
+
+//set - kümeler
+// const mySet = new Set();
+// mySet.add(50);
+// mySet.add("Deneme");
+// mySet.add([1, 2, 3]);
+
+// console.log(mySet);
+// console.log(typeof mySet);
+
+// const arr = [1, 2, 7, 5, 4, 2, 1];
+// const arrSet = new Set(arr);
+
+// console.log(arrSet);
+// console.log(typeof arrSet);
+// console.log(arrSet.has(8));
+
+// for of
+
+// for (let value of arrSet) {
+//   console.log(value);
+// }
+
+//foreach
+// arrSet.forEach((value) => console.log(value));
