fde6d9a8ee9165dd6098da548bc3ef35a3d20f93
ilaydeon <karaceperilayda@gmail.com>
2022-11-30 16:14:59 +0300

    projeler

---
+++ Ogrenciler/ilaydaKaraceper/javascript/app.js
@@ -0,0 +1,44 @@
+// let number1, number2, kalanlar;
+// let arr = [100, 200, 300, 400];
+// // number1 = arr[0];
+// // number2 = arr[1];
+
+// // Destructing
+// // [number1, number2] = arr;
+
+// [number1, number2, ...kalanlar]= arr;
+// console.log(number1, number2, kalanlar);
+
+// const numbers = { a: 10, b:20, c:30, d:40, e:50};
+
+// const{a:n1, c:n2, e: n3}=numbers;
+// console.log(n1, n2, n3);
+
+// const{name:isim, 
+//     year:yil, 
+//     salary:maas, 
+//     showInfos:bilgileriGoster,} =person;
+
+// console.log(isim, yil, maas);
+// bilgileriGoster();
+
+// const person ={
+//     name:"Varol Maksutoğlu",
+//     year:1985,
+//     salary: 5000,
+//     showInfos: () => console.log("Bilgiler Gösteriliyor..."),
+// };
+// const langs = {"Python", "C++", "Go","Ruby","JavaScript"};
+
+// // For in
+
+// for (let prop in person) {
+//     console.log(prop, person[prop]);
+// }
+
+// for (let i in langs) {
+//     console.log(i, langs[i]);
+// }
+// for (let value of person) {
+//     console.log(value);
+// }
