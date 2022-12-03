92da2b8877c4ed968cf9830dac6c10df494882c1
ersankocabiyik <ersankcbyk@gmail.com>
2022-12-03 14:25:50 +0300

    2

--- Ogrenciler/Ersan/Ders17/Uygulama/project.js
+++ Ogrenciler/Ersan/Ders17/Uygulama/project.js
@@ -6,11 +6,23 @@ const cardbody = document.querySelectorAll(".card-body")[1];
 const clear = document.getElementById("clear-films");
 
 const ui = new UI();
+const storage = new Storage();
 eventListeners();
 function eventListeners() {
   form.addEventListener("submit", addFilm);
   cardbody.addEventListener("click", deleteFilm);
+  document.addEventListener("DOMContentLoaded", loadAllFilms);
+  clear.addEventListener("click", clearAll);
 }
+function clearAll(e) {
+  ui.cleatAllFilmsFromUI();
+  storage.deleteFilmsFromStorage();
+}
+function loadAllFilms() {
+  let filmler = storage.getFilmsFromStorage();
+  filmler.forEach((film) => ui.addFilmToUI(film));
+}
+
 function addFilm(e) {
   const title = titleElement.value;
   const director = directorElement.value;
@@ -20,14 +32,19 @@ function addFilm(e) {
   } else {
     const newFilm = new Film(title, director, url);
     ui.addFilmToUI(newFilm);
+    storage.addFilmToStorage(newFilm);
     ui.displayMessage("Film başarılı bir şekilde eklendi", "success");
   }
-
   e.preventDefault();
 }
 function deleteFilm(e) {
+  filmAdi =
+    e.target.parentElement.previousElementSibling.previousElementSibling
+      .textContent;
+
   if (e.target.id === "delete-film") {
     ui.deleteFilmFromUI(e.target);
+    storage.deleteFilmFromStorage(filmAdi);
     ui.displayMessage("Film silme başarılı", "success");
   }
 }
