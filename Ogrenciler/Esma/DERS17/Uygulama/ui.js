4c854cb73beaaa719fd55cc2e72b4d067dbac4e2
eakul <97313237+eakul@users.noreply.github.com>
2022-12-04 10:07:51 +0100

    DERS17Barısla Uygulama

--- Ogrenciler/Esma/DERS17/Uygulama/ui.js
+++ Ogrenciler/Esma/DERS17/Uygulama/ui.js
@@ -1,8 +1,8 @@
-function UI() {}
-UI.prototype.addFilmToUI = function (newFilm) {
-  const filmList = document.getElementById("films");
-  
-  filmList.innerHTML += `
+class UI {
+  static addFilmToUI(newFilm) {
+    const filmList = document.getElementById("films");
+
+    filmList.innerHTML += `
    <tr>
      <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
      <td>${newFilm.title}</td>
@@ -10,29 +10,35 @@ UI.prototype.addFilmToUI = function (newFilm) {
      <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
    </tr>
   `;
-};
-UI.prototype.clearInputs = function (elemen1, elemen2, elemen3) {
-  elemen1.value = "";
-  elemen2.value = "";
-  elemen3.value = "";
-};
-
-UI.prototype.deleteFilmFromUI = function (e) {
-  e.parentElement.parentElement.remove();
-};
-UI.prototype.cleatAllFilmsFromUI = function () {
-  const filmList = document.getElementById("films");
-  while (filmList.firstElementChild !== null) {
-    filmList.firstElementChild.remove();
   }
-};
-UI.prototype.displayMessage = function (message, type) {
-  const cardBody = document.querySelector(".card-body");
-  const div = document.createElement("div");
-  div.className = `alert alert-${type}`;
-  div.textContent = message;
-  cardBody.appendChild(div);
-  setTimeout(function () {
-    div.remove();
-  }, 2000);
-};
+  static clearInputs(elemen1, elemen2, elemen3) {
+    elemen1.value = "";
+    elemen2.value = "";
+    elemen3.value = "";
+  }
+
+  static deleteFilmFromUI(e) {
+    e.parentElement.parentElement.remove();
+  }
+  static cleatAllFilmsFromUI() {
+    const filmList = document.getElementById("films");
+    while (filmList.firstElementChild !== null) {
+      filmList.firstElementChild.remove();
+    }
+  }
+  static displayMessage(message, type) {
+    const cardBody = document.querySelector(".card-body");
+    const div = document.createElement("div");
+    div.className = `alert alert-${type}`;
+    div.textContent = message;
+    cardBody.appendChild(div);
+    setTimeout(function () {
+      div.remove();
+    }, 2000);
+  }
+  static filterFilmsUI() {
+    const ara = document.getElementById("textara");
+    const tr = document.getElementById("films");
+    console.log(tr);
+  }
+}
