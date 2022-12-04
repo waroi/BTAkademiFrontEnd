373efba5d7a73f2a72a009d624b0e483727531c0
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2022-12-04 12:18:21 +0300

    HttpRequest

---
+++ Dersler/Ders19-Ajax/Uygulama/app.js
@@ -0,0 +1,7 @@
+function change() {
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://api.apilayer.com/exchangerates_data/latest?apikey=vyr0d4PsHNN852X4jUrIXA4RWSCELkLZ"
+  );
+}
