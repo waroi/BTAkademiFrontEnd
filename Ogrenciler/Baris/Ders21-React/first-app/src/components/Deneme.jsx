84a1f581f6e90250c0effae482b88ccc8b427b2b
bbssyl <bb.sosyal@pm.me>
2022-12-19 19:04:28 +0300

    Update Deneme.jsx

--- Ogrenciler/Baris/Ders21-React/first-app/src/components/Deneme.jsx
+++ Ogrenciler/Baris/Ders21-React/first-app/src/components/Deneme.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Deneme({name}) {
+export default function Deneme({isim}) {
   return (
-    <div>Bu bir test componentidir. Merhaba {name}</div>
+    <div>Bu bir test componentidir. Merhaba {isim}</div>
   )
 }
