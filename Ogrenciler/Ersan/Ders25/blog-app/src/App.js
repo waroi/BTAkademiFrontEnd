d1fbcff9c33a10099d242ee754e95b19d340d169
ersankocabiyik <ersankcbyk@gmail.com>
2023-01-07 14:00:36 +0300

    1

---
+++ Ogrenciler/Ersan/Ders25/blog-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { useRef, useEffect } from "react";
+
+function App() {
+  const inputRef=useRef();
+  useEffect(())
+  return (
+    <div className="App">
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
