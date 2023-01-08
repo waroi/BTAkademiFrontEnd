e0573962993ae851a5392aab25336b671ff6b9f6
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2023-01-08 12:32:29 +0300

    route

--- Dersler/Ders26-Router/router-app/src/routes/Router.jsx
+++ Dersler/Ders26-Router/router-app/src/routes/Router.jsx
@@ -6,6 +6,7 @@ import userRouter from "./userRouter";
 import ParametreView from "../views/ParametreView";
 import SportsNewView from "../views/SportsNewView";
 import EconomyNewView from "../views/EconomyNewView";
+import SportParam from "../views/SportParam";
 
 const Router = () => {
   const routes = useRoutes([
@@ -17,7 +18,11 @@ const Router = () => {
       element: <NewView />,
       children: [
         { index: true, element: <Navigate to="/news/sports" /> },
-        { path: "sports", element: <SportsNewView /> },
+        {
+          path: "sports",
+          element: <SportsNewView />,
+          children: [{ path: ":name", element: <SportParam /> }],
+        },
         { path: "economy", element: <EconomyNewView /> },
       ],
     },
