4cc9232803c13a1baeace567d7fa4a8dafa1c153
hakanmolla <75783413+hakanmolla@users.noreply.github.com>
2023-01-29 10:53:33 +0300

    react

--- Ogrenciler/HakanMolla/ders27-tekrar/tekrar-app/src/App.js
+++ Ogrenciler/HakanMolla/ders27-tekrar/tekrar-app/src/App.js
@@ -1,40 +1,53 @@
-
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '74ce2f5724msh1e99219984ad861p14e2a5jsn85a0fac159b3',
-      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "74ce2f5724msh1e99219984ad861p14e2a5jsn85a0fac159b3",
+      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+    },
   };
-  
-  fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  useEffect(() => {
+    getData();
+  }, []);
 
+  const getData = () => {
+    fetch(
+      "https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setData(response);
+      })
+      .catch((err) => console.error(err));
+  };
 
-
-
+  console.log("***");
+  //  console.log(options);
   return (
+
+
+
     <div className="App">
-
-
-
-
-
-      <ul>
-        <li>Test-1</li>
-        <li>Test-1</li>
-        <li>Test-1</li>
-        <li>Test-1</li>
-        <li>Test-1</li>
-        <li>Test-1</li>
-
-      </ul>
+      <div className="card" style={{width: "18rem",}}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- Ogrenciler/HakanMolla/ders27-tekrar/tekrar-app/src/index.js
+++ Ogrenciler/HakanMolla/ders27-tekrar/tekrar-app/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
+    
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
-
