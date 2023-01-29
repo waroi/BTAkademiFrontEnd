16cc46a51e829ed28c822a05b693197646908129
Varol Maksutoglu <varolmaksutoglu@yandex.com>
2023-01-29 13:34:42 +0300

    contextapi

--- Dersler/Ders27-Tekrar/tekrar-app/src/App.js
+++ Dersler/Ders27-Tekrar/tekrar-app/src/App.js
@@ -20,13 +20,7 @@ function App() {
 
   return (
     <div className="App">
-     <ul>
-      <li>Test 1</li>
-      <li>Test 2</li>
-      <li>Test 3</li>
-      <li>Test 4</li>
-      <li>Test 5</li>
-     </ul>
+     Deneme
     </div>
   );
 }
