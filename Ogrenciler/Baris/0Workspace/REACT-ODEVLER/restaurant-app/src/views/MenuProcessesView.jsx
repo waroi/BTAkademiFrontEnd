5b8ec0dec3d4e8f89b28814c57e25b05e4dd5e59
Barış Benli <bb.sosyal@pm.me>
2023-02-01 01:07:04 +0300

    restaurant app update

--- Ogrenciler/Baris/0Workspace/REACT-ODEVLER/restaurant-app/src/views/MenuProcessesView.jsx
+++ Ogrenciler/Baris/0Workspace/REACT-ODEVLER/restaurant-app/src/views/MenuProcessesView.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Badge, Card, Col, Row } from "react-bootstrap";
+import { useCategory } from "../context/CategoryContext";
+import { useProduct } from "../context/ProductContext";
 
 const MenuProcessesView = () => {
-  return <div>MenuProcessesView</div>;
+  const { products } = useProduct();
+  const { categories } = useCategory();
+  return (
+    <Row md={4} className="g-4">
+      {products.map((product) => {
+        return (
+          <Col key={product.id}>
+            <Card className="h-100">
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text className="fw-light fst-italic">
+                  {product.description}
+                </Card.Text>
+                <Card.Text>Fiyat:{product.price}₺</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                Stok Durumu:
+                {product.visible ? (
+                  <Badge bg="success">Var</Badge>
+                ) : (
+                  <Badge bg="danger">YOK</Badge>
+                )}
+              </Card.Footer>
+            </Card>
+          </Col>
+        );
+      })}
+    </Row>
+  );
 };
 
 export default MenuProcessesView;
